3712b6d45c51ed34aeef2fab1536f08532482f4c
Note Soumu <soumunote@gmail.com>
2021-05-01 06:21:33 +0900

    Stepper途中

--- components/stepper.js
+++ components/stepper.js
@@ -2,6 +2,11 @@ import React from 'react';
 import 'node_modules/bs-stepper/dist/css/bs-stepper.min.css';
 import BsStepper from 'bs-stepper';
 
+class Step extends React.Component {
+
+
+}
+
 class Stepper extends React.Component {
 
   constructor(props) {
@@ -19,13 +24,28 @@ class Stepper extends React.Component {
     return (
       <div id={this.props.name} className="bs-stepper">
         <div className="bs-stepper-header" role="tablist">
-          
+          {
+            this.props.children
+              .filter(step => step instanceof Step)
+              .map(step => 
+                <div class="step" data-target={`#${this.props.id}.content`}>
+                  <button type="button" class="step-trigger" role="tab" arial-controls={`${this.props.}.content`} id={`${this.props.id}.trigger`}>
+                    <span class="bs-stepper-circle">{step.props.no}</span>
+                    <span class="bs-stepper-label">{step.props.label}</span>
+                  </button>
+                </div>
+              })
+          }
         </div>
         <div className="bs-stepper-contnt">
           {
-            this.props.children.map(step => {
-              
-            })
+            this.props.children
+              .filter(step => step instanceof Step)
+              .map(step => {
+                <div id={`${this.props.id}.content`} class="content" role="tabpanel" aria-labelledby={`${this.props.id}.trigger`}>
+                  {step.children}
+                </div>
+              })
           }
         </div>
       </div>
