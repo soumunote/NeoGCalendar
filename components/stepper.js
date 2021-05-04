3fdf8f38d4ac7280392a5251d5f44d9c162cc0fb
Note Soumu <soumunote@gmail.com>
2021-05-04 22:44:31 +0900

    Stepperは大体よし

--- components/stepper.js
+++ components/stepper.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import 'node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'node_modules/bs-stepper/dist/css/bs-stepper.min.css';
 import BsStepper from 'bs-stepper';
 
 class Step extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    return (
+      <div id={this.props.id} className="content" role="tabpanel" aria-labelledby={`${this.props.id}.trigger`}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
 
+Step.propTypes = {
+  id: PropTypes.string.isRequired,
+  no: PropTypes.number.isRequired,
+  label: PropTypes.string.isRequired,
 }
 
 class Stepper extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   componentDidMount() {
-    this.stepper = new BsStepper(document.querySelector(`#${this.props.name}`), {
+    this.stepper = new BsStepper(document.querySelector(`#${this.props.id}`), {
       linear: false,
       Animation: true,
     });
@@ -22,33 +38,36 @@ class Stepper extends React.Component {
 
   render() {
     return (
-      <div id={this.props.name} className="bs-stepper">
+      <div id={this.props.id} className="bs-stepper">
         <div className="bs-stepper-header" role="tablist">
           {
-            this.props.children
-              .filter(step => step instanceof Step)
-              .map(step => 
-                <div class="step" data-target={`#${this.props.id}.content`}>
-                  <button type="button" class="step-trigger" role="tab" arial-controls={`${this.props.}.content`} id={`${this.props.id}.trigger`}>
-                    <span class="bs-stepper-circle">{step.props.no}</span>
-                    <span class="bs-stepper-label">{step.props.label}</span>
-                  </button>
-                </div>
-              })
+            React.Children.map(this.props.children, step => 
+              <div className="step" data-target={`#${step.props.id}`}>
+                <button type="button" className="step-trigger" role="tab" arial-controls={step.props.id} id={`${step.props.id}.trigger`}>
+                  <span className="bs-stepper-circle">{step.props.no}</span>
+                  <span className="bs-stepper-label">{step.props.label}</span>
+                </button>
+              </div>
+            ).join(<div classNmae="line"></div>)
           }
         </div>
         <div className="bs-stepper-contnt">
           {
-            this.props.children
-              .filter(step => step instanceof Step)
-              .map(step => {
-                <div id={`${this.props.id}.content`} class="content" role="tabpanel" aria-labelledby={`${this.props.id}.trigger`}>
-                  {step.children}
-                </div>
-              })
+            React.Children.map(this.props.children, content =>
+              <div id={content.props.id} className="content" role="tabpanel" aria-labelledby={`${content.props.id}.trigger`}>
+                {content.props.children}
+              </div>
+            )
           }
-        </div>
+        </div>
       </div>
     );
   }
 }
+
+Stepper.propTypes = {
+
+}
+
+export default Stepper;
+export { Step };
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+import dynamic from 'next/dynamic';
+const Stepper = dynamic(
+  () => import('components/stepper'),
+  { ssr: false }
+);
+const Step = dynamic(
+  () => import('components/stepper').then((mod) => mod.Step),
+  { ssr: false }
+);
+
 function Index() {
-  
+  return (
+    <Stepper id="main">
+      <Step key="1" id="dneo" no="1" label="desknet's NEO">
+        Let's login to desknet's NEO.
+      </Step>
+      <Step key="2" id="google" no="2" label="Google">
+        Let's login to Google. 
+      </Step>
+    </Stepper>
+  );
 }
 
 export default Index;
